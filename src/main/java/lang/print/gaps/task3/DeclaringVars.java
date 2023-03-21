b25b0fecebca9b01a1d517af8c7c4b81edd7443a
LVLAD2009@yandex.ru <24233lebedko>
2023-03-21 23:06:45 +0300

    stage 0 modul 3 ex 3

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -4,6 +4,7 @@ public class DeclaringVars {
     public static void main(String[] args) {
         int a, b, c;
         a = b = c = 10;
+
         System.out.println(c);
         //write code here
     }
